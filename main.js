1916d634ec41c5f9c784ed6963a49f70e54a49fe
urumci <nijatt23@gmail.com>
2024-11-10 19:55:52 +0200

    log

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+//filterMenu
 function filterMenu(category) {
     const cards = document.querySelectorAll('.menu-card');
     cards.forEach(card => {
@@ -10,3 +10,19 @@ function filterMenu(category) {
         }
     });
 }
+
+
+//features 
+let currentIndex = 0;
+
+function moveCarousel() {
+    const track = document.querySelector('.carousel-track');
+    const items = document.querySelectorAll('.carousel-item');
+    const itemWidth = items[0].getBoundingClientRect().width;
+    
+    currentIndex = (currentIndex + 1) % items.length;
+    const newTransform = -currentIndex * itemWidth;
+    track.style.transform = `translateX(${newTransform}px)`;
+}
+
+setInterval(moveCarousel, 5000); // Auto-scroll every 5 seconds
